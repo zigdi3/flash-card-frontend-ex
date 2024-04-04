f8c3aa81c9b6acf2a5912b3d34f27cb882597eb8
Diego Araujo <diegozigoto@gmail.com>
2024-04-03 23:45:42 -0300

    Labels e estilos (#1)
    * Labels e estilos
    
    * disable WIP themes
    
    * remove import

--- src/app/pages/card-view/card-view.component.ts
+++ src/app/pages/card-view/card-view.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { CardService } from '../services/card.service';
 import { Router } from '@angular/router';
+import { CardService } from '../services/card.service';
 
 @Component({
   selector: 'app-card-view',
   templateUrl: './card-view.component.html',
-  styleUrls: ['./card-view.component.scss']
+  styleUrls: ['./card-view.component.scss'],
 })
 export class CardViewComponent implements OnInit {
   public cardList: any[] = [];
-  constructor(private cardService: CardService,
-    private route: Router) { }
+  constructor(
+    private cardService: CardService,
+    private route: Router
+  ) // private themeService: ThemeService
+  {
+    // this.themeService.themeChange.subscribe({
+    //   next: (theme: string) => {
+    //     console.log("listen themes from app...");
+    //   }
+    // });
+  }
 
   ngOnInit() {
-
-    this.cardService.list().subscribe(res => {
+    this.cardService.list().subscribe((res) => {
       next: this.cardList = res.data;
-      error: () => console.log("Erro ao listar cards");
-
-    }
-    );
+      error: () => console.log('Erro ao listar cards');
+    });
   }
   goToRegisterCard() {
     this.route.navigate(['card-register']);
   }
-
-
 }
