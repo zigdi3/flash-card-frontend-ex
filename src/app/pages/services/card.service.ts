11f8081b117a7b619457b9d6f6958a4a58c8d366
Diego Araujo <diegozigoto@gmail.com>
2025-02-23 18:44:03 -0300

    🚅 perfs: Update Angular 17

--- src/app/pages/services/card.service.ts
+++ src/app/pages/services/card.service.ts
@@ -1,15 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
 import { APP_CONFIG } from 'src/assets/config/app.config';
 import { CardProfile } from '../card-register/model/card-profile.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CardService {
   url: string = APP_CONFIG.API_SERVER;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   list(): Observable<any> {
     const res = this.http.get(this.url + `/v1/flash-card/list`);
@@ -18,7 +17,6 @@ export class CardService {
 
   save(input: CardProfile): Observable<any> {
     const res = this.http.post(this.url + `/v1/flash-card/save`, input);
-    return res
+    return res;
   }
-
 }
